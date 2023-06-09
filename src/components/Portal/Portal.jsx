f47857c02421aa40a777adea0fe2ac11af255ab7
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-09 17:05:40 +0300

    fix export with canvas

--- src/components/Portal/Portal.jsx
+++ src/components/Portal/Portal.jsx
@@ -1,6 +1,6 @@
 import { useRef } from 'react';
 import { Color } from 'three';
-import { Canvas, extend, useFrame, useThree } from '@react-three/fiber';
+import { Canvas, extend, useFrame } from '@react-three/fiber';
 import { shaderMaterial } from '@react-three/drei';
 
 import PlaneMesh from '../PlaneMesh';
@@ -11,21 +11,18 @@ extend({
   PortalMaterial: shaderMaterial({ uTime: 0, uColorStart: new Color('#ffffff'), uColorEnd: new Color('#000000') }, vertexShader, fragmentShader),
 });
 
-export default function Portal() {
-  const { viewport } = useThree();
+export function PortalMaterial() {
   const portalMaterialRef = useRef();
+  useFrame((state, delta) => { portalMaterialRef.current.uTime += delta; });
+  return <portalMaterial ref={portalMaterialRef} />;
+}
 
-  useFrame((state, delta) => {
-    portalMaterialRef.current.uTime += delta;
-  });
-
+export default function Portal() {
   return (
     <Canvas>
-      <mesh scale={[viewport.width, viewport.height, 1]}>
-        <planeGeometry />
-        <portalMaterial ref={portalMaterialRef} />
-        {children}
-      </mesh>
+      <PlaneMesh>
+        <PortalMaterial />
+      </PlaneMesh>
     </Canvas>
   );
 }
