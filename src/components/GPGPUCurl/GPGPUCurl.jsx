9e2596482959f1a99294f6dc868670131cea8cdb
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-09 17:00:42 +0300

    add canvas

--- src/components/GPGPUCurl/GPGPUCurl.jsx
+++ src/components/GPGPUCurl/GPGPUCurl.jsx
@@ -1,6 +1,6 @@
 import { useMemo, useState, useRef } from 'react';
 import * as THREE from 'three';
-import { createPortal, useFrame } from '@react-three/fiber';
+import { Canvas, createPortal, useFrame } from '@react-three/fiber';
 import { CameraShake, useFBO } from '@react-three/drei';
 import { useControls } from 'leva';
 
@@ -8,11 +8,27 @@ import { useControls } from 'leva';
 import './dofPointsMaterial';
 import './simulationMaterial';
 
-const Particles = ({ speed, fov, aperture, focus, curl, size = 512, ...props }) => {
-  // REFS
-  const simRef = useRef(),
-    renderRef = useRef();
-  // SET UP FBO
+export default function GPGPUCurl() {
+  const options = useControls({
+    focus: { value: 5.4, min: 3, max: 7, step: 0.01 },
+    speed: { value: 16.6, min: 0.1, max: 100, step: 0.1 },
+    aperture: { value: 5.1, min: 1, max: 5.6, step: 0.1 },
+    fov: { value: 0, min: 0, max: 200 },
+    curl: { value: 0.5, min: 0.01, max: 0.5, step: 0.01 },
+  });
+
+  return (
+    <Canvas linear={true} camera={{ fov: 25 }} position={[0, 0, 6]} gl={{ antialias: true, alpha: true }}>
+      <CameraShake yawFrequency={1} maxYaw={0.05} pitchFrequency={1} maxPitch={0.05} rollFrequency={0.5} maxRoll={0.5} intensity={0.2} />
+      <Particles {...options} />
+    </Canvas>
+  );
+}
+
+export function Particles({ speed, fov, aperture, focus, curl, size = 512, ...props }) {
+  const simRef = useRef();
+  const renderRef = useRef();
+  // Set up FBO
   const [scene] = useState(() => new THREE.Scene());
   const [camera] = useState(() => new THREE.OrthographicCamera(-1, 1, 1, -1, 1 / Math.pow(2, 53), 1));
   const [positions] = useState(() => new Float32Array([-1, -1, 0, 1, -1, 0, 1, 1, 0, -1, -1, 0, 1, 1, 0, -1, 1, 0]));
@@ -23,7 +39,7 @@ const Particles = ({ speed, fov, aperture, focus, curl, size = 512, ...props }) 
     format: THREE.RGBAFormat,
     type: THREE.FloatType,
   });
-  // NORMALIZE POINTS
+  // Normalize points
   const particles = useMemo(() => {
     const length = size * size;
     const particles = new Float32Array(length * 3);
@@ -34,8 +50,7 @@ const Particles = ({ speed, fov, aperture, focus, curl, size = 512, ...props }) 
     }
     return particles;
   }, [size]);
-
-  // UPDATE FBO AND POINTCLOUD EVERY FRAME
+  // Update FBO and pointcloud every frame
   useFrame((state) => {
     state.gl.setRenderTarget(target);
     state.gl.clear();
@@ -49,7 +64,6 @@ const Particles = ({ speed, fov, aperture, focus, curl, size = 512, ...props }) 
     simRef.current.uniforms.uTime.value = state.clock.elapsedTime * speed;
     simRef.current.uniforms.uCurlFreq.value = THREE.MathUtils.lerp(simRef.current.uniforms.uCurlFreq.value, curl, 0.1);
   });
-
   return (
     <>
       {/* Simulation goes into a FBO/Off-buffer */}
@@ -70,23 +84,6 @@ const Particles = ({ speed, fov, aperture, focus, curl, size = 512, ...props }) 
           <bufferAttribute attach='attributes-position' count={particles.length / 3} array={particles} itemSize={3} />
         </bufferGeometry>
       </points>
-    </>
-  );
-};
-
-export default function GPGPUCurl() {
-  const options = useControls({
-    focus: { value: 5.4, min: 3, max: 7, step: 0.01 },
-    speed: { value: 16.6, min: 0.1, max: 100, step: 0.1 },
-    aperture: { value: 5.1, min: 1, max: 5.6, step: 0.1 },
-    fov: { value: 0, min: 0, max: 200 },
-    curl: { value: 0.5, min: 0.01, max: 0.5, step: 0.01 },
-  });
-
-  return (
-    <>
-      <CameraShake yawFrequency={1} maxYaw={0.05} pitchFrequency={1} maxPitch={0.05} rollFrequency={0.5} maxRoll={0.5} intensity={0.2} />
-      <Particles {...options} />
     </>
   );
 }
